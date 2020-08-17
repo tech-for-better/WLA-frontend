b8f53c3577f2fc7adeb25b7b1060b6964d19888a
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-17 18:26:57 +0100

    chore: add bootstrap 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import Title from '@/components/Title';
 
 const Home: React.FC<PageProps> = () => (
   <main>
     <Title />
+    <Button variant="dark">Primary</Button>
     <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
     <p>
       Follow me on Twitter (
