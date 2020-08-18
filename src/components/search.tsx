27b86d251fa5a2da14bd9359ac0cf6e86aed5279
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-18 17:38:05 +0100

    chore(search.tsx): add search bar, sort by dropdown, type, switch and range elements Relates #27

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,5 +1,61 @@
 import React from 'react';
 
-const Search: React.FC<{}> = () => <h1>Hello TypeScript!</h1>;
+// react-booststrap imoports
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
+const Search: React.FC<{}> = () => (
+  <>
+    <Form>
+      <Form.Row>
+        <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-success">Search</Button>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>Sort By:</Form.Label>
+          <Form.Control as="select" defaultValue="Choose...">
+            <option value="0">Choose...</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Type:</Form.Label>
+          <Form.Control
+            as="select"
+            className="mr-sm-2"
+            id="inlineFormCustomSelect"
+            custom
+          >
+            <option value="0">Choose...</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Check
+            type="switch"
+            id="custom-switch"
+            label="Check this switch"
+          />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group as={Col} controlId="formBasicRange">
+        <Form.Label>Range</Form.Label>
+        <Form.Control type="range" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Search
+      </Button>
+    </Form>
+  </>
+);
 
 export default Search;
