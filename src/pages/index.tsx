a8dadba2f159a5fb2b51e76fb33ed8827492533e
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-18 16:44:05 +0100

    chore(search.tsx): initialise search component Relates #27

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import { PageProps } from 'gatsby';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import Search from '../components/search';
 
 const Home: React.FC<PageProps> = () => (
   <main>
-    <Button variant="dark">Primary</Button>
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
+    <Search />
   </main>
 );
 
