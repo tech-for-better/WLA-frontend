a475a8183c6526a2958157e5efdfc329d34b8a44
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-18 13:00:52 +0100

    refactor(layout.tsx): add bootstrap container to keep things inside the container Relates #1

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Nav from '../nav/nav';
 import Footer from '../footer/Footer';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <Container fluid="xl">
       <Nav />
       <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
         {children}
       </div>
       <Footer />
-    </>
+    </Container>
   );
 }
