2cdecd20d13de3bddb61b3e501ce3944f5512104
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-18 11:09:19 +0100

    chore(layout.tsx): add layout plugin and laout tsx file Relates #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,17 @@ import Button from 'react-bootstrap/Button';
 import Title from '@/components/Title';
 
 const Home: React.FC<PageProps> = () => (
-  <main>
-    <Title />
-    <Button variant="dark">Primary</Button>
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
-  </main>
+
+    <main>
+      <Title />
+      <Button variant="dark">Primary</Button>
+      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
+      <p>
+        Follow me on Twitter (
+        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
+      </p>
+    </main>
+
 );
 
 export default Home;
