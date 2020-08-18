2509426316f38d7b659725c8579f320841404d7b
Ivo Evans <ievans.storrie@gmail.com>
2020-08-18 17:24:17 +0100

    feat(Layout): Add Copy component w/ query + styles Relates #1

--- src/components/layouts/footer/Footer.tsx
+++ src/components/layouts/footer/Footer.tsx
@@ -3,20 +3,15 @@ import React from 'react';
 import * as SC from './styled-components';
 import logo from '../../../assets/SkillsWest.London.png';
 
+import Copy from './Copy';
 import ExternalLinks from './ExternalLinks';
 import InternalLinks from './InternalLinks';
 
 const Footer: React.FC<{}> = () => (
   <SC.StyledFooter>
     <SC.FirstSection>
-      <h3>Result of a page query</h3>
-      <p>
-        Lorem ipsum P dolor sit amet consectetur adipisicing elit. Impedit,
-        similique sapiente laboriosam illo nobis voluptate hic quas perspiciatis
-        veritatis, quibusdam distinctio? Doloremque ducimus assumenda unde hic
-        sequi accusantium inventore ex.
-      </p>
       <SC.Logo alt="SkillsWest.London logo" src={logo} />
+      <Copy />
     </SC.FirstSection>
     <SC.Separator />
     <SC.SecondSection>
--- src/components/layouts/footer/styled-components.tsx
+++ src/components/layouts/footer/styled-components.tsx
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 import styles from '../../../styles';
 
+const mediaQuery = (content) => `
+@media only screen and (max-width: ${styles.breakpoint}) {
+${content}
+}
+`;
+
 export const StyledFooter = styled.footer`
   padding: 2em;
   display: grid;
@@ -10,41 +16,61 @@ export const StyledFooter = styled.footer`
   background-color: ${styles.grey};
   color: #fff;
 
-  @media only screen and (max-width: ${styles.breakpoint}) {
+  ${mediaQuery(`{
     grid-template-columns: 1fr;
     grid-template-rows: 1fr auto 1fr;
-  }
+  }`)}
 `;
 
-export const FirstSection = styled.section``;
+export const FirstSection = styled.section`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 export const Logo = styled.img`
-  width: 5em;
-  max-width: 50%;
-  height: auto;
+  width: auto;
+  height: 1em;
+  margin-right: 1em;
+`;
+
+export const Strapline = styled.h3`
+  color: #fff;
+  font-size: ${styles.font[1]};
+`;
+
+export const Details = styled.p`
+  color: #fff;
+  font-size: ${styles.font[0]};
 `;
 
 export const Separator = styled.div`
   width: 1px;
-  // there is padding
+  // there is padding, so 100% is sufficiently small
   height: 100%;
   background-color: #fff;
   margin: 0 1em;
 
-  @media only screen and (max-width: ${styles.breakpoint}) {
+  ${mediaQuery(`
+  {
     width: 100%;
     height: 1px;
     margin: 1em 0;
   }
+  `)}
 `;
 
 export const SecondSection = styled.section`
   display: flex;
+
+  ${mediaQuery(`
+  justify-content: space-around;
+  `)}
 `;
 
 const linkStyling = `
 display: block;
 color: #fff;
+margin: 1em 0;
 font-size: ${styles.font[0]};
 :hover {
   color: #fff;
@@ -61,7 +87,4 @@ export const ExternalLink = styled.a`
 
 export const LinksList = styled.div`
   margin-left: 2em;
-  /* display: flex;
-flex-direction: column;
-justify-content: space-around; */
 `;
