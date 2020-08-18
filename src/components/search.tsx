272bbaba916bccc17757707b8111cae8112662d9
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-18 17:52:58 +0100

    chore(search.tsx): add correct value and labels on the the form elements Relates #24

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -9,17 +9,18 @@ const Search: React.FC<{}> = () => (
   <>
     <Form>
       <Form.Row>
+        {/* search button needs to go next to the bar and maybe make it bigger */}
         <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
         <Button variant="outline-success">Search</Button>
       </Form.Row>
       <Form.Row>
         <Form.Group as={Col} controlId="formGridState">
           <Form.Label>Sort By:</Form.Label>
-          <Form.Control as="select" defaultValue="Choose...">
-            <option value="0">Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <Form.Control as="select" defaultValue="Relevance">
+            <option value="0">Start Date</option>
+            <option value="1">Price: Low to High</option>
+            <option value="2">Price: High to Low</option>
+            <option value="3">Relevance</option>
           </Form.Control>
         </Form.Group>
 
@@ -31,29 +32,22 @@ const Search: React.FC<{}> = () => (
             id="inlineFormCustomSelect"
             custom
           >
-            <option value="0">Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+            <option value="0">Full-Time Campus</option>
+            <option value="1">Part-Time Campus</option>
+            <option value="2">Online - Full-Time</option>
+            <option value="3">Online - Part-Time</option>
           </Form.Control>
         </Form.Group>
         <Form.Group as={Col}>
-          <Form.Check
-            type="switch"
-            id="custom-switch"
-            label="Check this switch"
-          />
+          {/* needs vertical positioning */}
+          <Form.Check type="switch" id="custom-switch" label="Remote" />
         </Form.Group>
       </Form.Row>
 
       <Form.Group as={Col} controlId="formBasicRange">
-        <Form.Label>Range</Form.Label>
+        <Form.Label>Dificulty Range</Form.Label>
         <Form.Control type="range" />
       </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Search
-      </Button>
     </Form>
   </>
 );
