42385ee94f577d18ce7299c210c4af135eb8ed23
Ivo Evans <ievans.storrie@gmail.com>
2020-08-18 11:02:50 +0100

    chore(storybook): Install storybook

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Title: React.FC<{}> = () => <footer>Footer</footer>;
+
+export default Title;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PageProps } from 'gatsby';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import Title from '@/components/Title';
+import Title from '~/src/components/Title';
 
 const Home: React.FC<PageProps> = () => (
   <main>
