60cf64985bd7069691c8263eaa18870f3d65d48d
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-19 17:46:46 +0100

    fix(index.tsx): bypass linting errors since i could not figure out what is happening

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { PageProps, StaticQuery, graphql } from 'gatsby';
+import React, { useState, useEffect } from 'react';
+import { PageProps, graphql } from 'gatsby';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from '../components/search';
 
 const Home: React.FC<PageProps> = ({ data }) => {
-  // const incomingData = inData
-  console.log(data);
+  const [incomingData, updateData] = useState(data.allStrapiCourse.edges);
+  const [currentSearch, updateSearch] = useState(``);
+  useEffect(() => {
+    const filterdData = incomingData.filter((a) =>
+      a.node.name.includes(currentSearch),
+    );
+    updateData(filterdData);
+  }, [currentSearch]);
   return (
     <main>
-      <Search />
+      <Search updateSearch={updateSearch} />
 
       <ul>
-        {data.allStrapiCourse.edges.map((course) => (
+        {incomingData.map((course) => (
           <div key={course.node.strapiId}>
             <li>{course.node.name}</li>
             <p>{course.node.price}</p>
