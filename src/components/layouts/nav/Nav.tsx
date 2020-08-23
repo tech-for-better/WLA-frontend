236ab1f04aabd894fd22751bd28440d1e8023eff
Ivo Evans <ievans.storrie@gmail.com>
2020-08-23 12:28:18 +0100

    feat(Nav): Put logo in navbar

--- src/components/layouts/nav/Nav.tsx
+++ src/components/layouts/nav/Nav.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 // botstrap imports
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import logo from '../../../assets/SkillsWest.London.png';
+
+// it didn't look to bad big either... but I anticipate a busy UI
+const Logo = styled.img`
+  width: auto;
+  max-height: 1.5em;
+`;
+
 const CustomNav: React.FC<{}> = () => {
   return (
     <Navbar expand="lg">
       <Link className="nav-link navbar-brand" to="/">
-        WLA
+        <Logo src={logo} alt="SkillsWest.London logo, and link to homepage" />
       </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
