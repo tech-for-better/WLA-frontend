bb18dc8d5f15a018987008b756538ffd7f69ae68
Ivo Evans <ievans.storrie@gmail.com>
2020-09-02 15:29:34 +0100

    fix(about): Images don't squash anymore - they resize for a while and then stack Closes #93

--- src/components/About/Copy/Article.tsx
+++ src/components/About/Copy/Article.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import styles from '../../../styles';
+import styles, { mediaQuery } from '../../../styles';
 
 interface ArticleProps {
   alignment: boolean;
@@ -9,41 +9,55 @@ interface ArticleProps {
   image: {
     link: string;
     description: string;
-  };
+  } | null;
 }
 
 const StyledArticle = styled.article`
   margin-top: 5em;
+  padding: 0 1em;
   display: flex;
   flex-direction: ${(props) => {
     return props.alignment ? `row-reverse` : `row`;
   }};
+  ${mediaQuery(`
+  flex-direction: column;
+
+  `)}/* flex-wrap: wrap; */
 `;
 
 const Title = styled.h1`
   text-align: center;
   font-size: ${styles.font[3]};
+  padding-bottom: 0.5em;
+`;
+
+const ImageBox = styled.div`
+  width: 100%;
+  height: auto;
 `;
 
 const Image = styled.img`
-  object-fit: cover;
+  width: 100%;
+  height: auto;
   ${(props) => {
     return props.alignment || `margin-right: 1em;`;
   }}
 `;
 
-const Article: React.FC<ArticleProps> = ({ title, text, image = null, alignment = null }) => {
+const Article: React.FC<ArticleProps> = ({ title, text, image = null, alignment = false }) => {
   // const { link, description } = image;
   return (
     <StyledArticle alignment={alignment}>
       {image && (
-        <Image
-          src={image.link}
-          alt={image.description}
-          width={300}
-          height={300}
-          alignment={alignment}
-        />
+        <ImageBox>
+          <Image
+            src={image.link}
+            alt={image.description}
+            width={300}
+            height={300}
+            alignment={alignment}
+          />
+        </ImageBox>
       )}
       <div>
         <Title>{title}</Title>
