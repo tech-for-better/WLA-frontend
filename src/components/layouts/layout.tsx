b105f7e906fc61f08ae8cf479d7ad623a1f3f9d2
Ivo Evans <ievans.storrie@gmail.com>
2020-09-02 18:12:03 +0100

    feat(layout): Add document title Closes #2 Relates #3

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 import Nav from './nav/Nav';
 import Footer from './footer/Footer';
 import styles from '../../styles';
@@ -21,6 +22,10 @@ const Main = styled(Container)`
 export default function Layout({ children }) {
   return (
     <Page>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Skills West London</title>
+      </Helmet>
       <Main fluid="xl" className="mt-3">
         <Nav />
         <div>{children}</div>
